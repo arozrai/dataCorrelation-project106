f85d5747df9c76f21fb39502cc34787299837301
arozrai <72703446+arozrai@users.noreply.github.com>
2021-08-28 20:03:41 +0100

    Add files via upload

---
+++ project106.py
@@ -0,0 +1,25 @@
+import plotly.express as px 
+import csv 
+import numpy as np 
+
+def getDataSource(data_path): 
+    coffeeIntake = [] 
+    HoursSlept = [] 
+    with open(data_path) as csv_file: 
+        csv_reader = csv.DictReader(csv_file) 
+        for row in csv_reader: 
+            coffeeIntake.append(float(row["Coffee in ml"])) 
+            HoursSlept.append(float(row["sleep in hours"])) 
+    return {"x": coffeeIntake , "y": HoursSlept} 
+
+def findCorrelation(datasource): 
+    correlation = np.corrcoef(datasource["x"], datasource["y"]) 
+    print(correlation) 
+    print("Correlation between Coffee Intake vs Hours Slept :- \n--->",correlation[0,1]) 
+
+def setup(): 
+    data_path = "coffeIntakeVsHoursSlept.csv" 
+    datasource = getDataSource(data_path) 
+    findCorrelation(datasource) 
+
+setup()
