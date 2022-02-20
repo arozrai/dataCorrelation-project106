a2615f34913af537c0003232e1a1442dbc784b36
arozrai <72703446+arozrai@users.noreply.github.com>
2022-02-20 14:02:39 +0000

    Add files via upload

--- project106.py
+++ project106.py
@@ -16,6 +16,8 @@ def findCorrelation(datasource):
     correlation = np.corrcoef(datasource["x"], datasource["y"]) 
     print(correlation) 
     print("Correlation between Coffee Intake vs Hours Slept :- \n--->",correlation[0,1]) 
+    fig = px.scatter(x=datasource["x"], y=datasource["y"])
+    fig.show()
 
 def setup(): 
     data_path = "coffeIntakeVsHoursSlept.csv" 
